af430216ecc192a4e5f1690e168e4bd04e35b8f6
Kevin Smith <futuretense@gmail.com>
2021-02-23 20:14:23 -0800

    Created conditional for login or register

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,14 +1,23 @@
 import React from "react";
 
-function Form() {
+function Form(props) {
   return (
     <form className="form">
       <input type="text" placeholder="Username" />
       <input type="password" placeholder="Password" />
-      <input type="password" placeholder="Confirm Password" />
-      <button type="submit">Register</button>
+      {!props.isRegistered && (
+        <input type="password" placeholder="Confirm Password" />
+      )}
+
+      <button type="submit">{props.isRegistered ? "Login" : "Register"}</button>
     </form>
   );
 }
 
 export default Form;
+
+//Challenge: Without moving the userIsRegistered variable,
+//1. Show Login as the button text if userIsRegistered is true.
+//Show Register as the button text if userIsRegistered is false.
+//2. Only show the Confirm Password input if userIsRegistered is false.
+//Don't show it if userIsRegistered is true.
