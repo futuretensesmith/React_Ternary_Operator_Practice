6ef8799fb5fa6cc3185ae8ac555d3af8cd569ea9
Kevin Smith <futuretense@gmail.com>
2021-02-24 16:30:58 -0800

    More comments on Form  component

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 // userIsRegistered variable is passed through isRegistered prop
 //use ternary operator to determine rendering
+//confirm password button is rendered if isRegistered is
+//false. writing it like {!props.isRegistered &&} and
+//using && operator is like asking is it true that it's false if
+//so execute the code on other side of &&. if not render null
+
 function Form(props) {
   return (
     <form className="form">
